ea1dd6683395cde06ec24c03f7c994464a6282e6
Eric Sun <eric.anpeng.sun@gmail.com>
2021-06-28 13:58:05 -0400

    Upload page with non-functioning form

--- trace-online/app/forms.py
+++ trace-online/app/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from flask_wtf.file import FileField, FileRequired
 
 class UploadForm(FlaskForm):
     name = StringField('Analysis Name')
-    data = FileField('Upload', validators=[FileRequired()])
+    file1 = FileField('Upload', validators=[FileRequired()])
+    file2 = FileField('Upload', validators=[FileRequired()])
+    submit = SubmitField('Submit')
--- trace-online/app/routes.py
+++ trace-online/app/routes.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import render_template
+from app.forms import UploadForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title="Home")
+
+@app.route('/upload', methods=["GET", "POST"])
+def upload():
+    form = UploadForm()
+    return render_template('upload.html', title="Upload", form=form)
