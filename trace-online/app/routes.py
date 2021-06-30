f17ef056dd422b15042119632b99b4166f55e398
Eric Sun <eric.anpeng.sun@gmail.com>
2021-06-30 16:49:00 -0400

    Added way for upload form data to be saved

--- trace-online/app/routes.py
+++ trace-online/app/routes.py
@@ -1,6 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, session, redirect, url_for
+from werkzeug.utils import secure_filename
 from app.forms import UploadForm
+import os
 
 @app.route('/')
 @app.route('/index')
@@ -10,4 +12,15 @@ def index():
 @app.route('/upload', methods=["GET", "POST"])
 def upload():
     form = UploadForm()
+    if form.validate_on_submit():
+        session['analysis name'] = form.name.data
+        file1 = form.file1.data
+        file2 = form.file2.data
+        filename1 = secure_filename(file1.filename)
+        filename2 = secure_filename(file2.filename)
+        file1.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads', filename1))
+        file2.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads', filename2))
+        session['filepath1'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads', filename1)
+        session['filepath2'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads', filename2)
+        return redirect(url_for('index'))
     return render_template('upload.html', title="Upload", form=form)
