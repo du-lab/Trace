d869bf952b380df0293288d5d5aa302ccf06ec66
Eric Sun <eric.anpeng.sun@gmail.com>
2021-07-06 11:26:26 -0400

    Parameters form

--- trace-online/app/forms.py
+++ trace-online/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField, DecimalField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms.validators import DataRequired
 
 class UploadForm(FlaskForm):
     name = StringField('Analysis Name')
@@ -12,4 +13,17 @@ class UploadForm(FlaskForm):
         FileRequired(),
         FileAllowed(['mzml'], 'mzML format only!')
     ])
+    submit = SubmitField('Submit')
+
+class ParametersForm(FlaskForm):
+    window_mz = IntegerField('m/z windows', validators=[DataRequired()])
+    window_rt = IntegerField('time window', validators=[DataRequired()])
+    mz_r = DecimalField('m/z tolerance', validators=[DataRequired()])
+
+    min_len_eic = IntegerField('EIC min length', validators=[DataRequired()])
+    window_size = IntegerField('Window size to calculate noise', validators=[DataRequired()])
+    min_snr = IntegerField('Min Signal-to-noise ratio', validators=[DataRequired()])
+    perc = IntegerField('Percentile within the window size', validators=[DataRequired()])
+    max_scale_for_peak = IntegerField('Max CWT scale', validators=[DataRequired()])
+
     submit = SubmitField('Submit')
